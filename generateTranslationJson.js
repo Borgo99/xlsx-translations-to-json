1de9b00786a09a89808015b3409acc357cfee2e4
Borgo99 <andrea123borgo@gmail.com>
2023-06-20 17:36:26 +0200

    First awesome commit

---
+++ generateTranslationJson.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+const fs = require("fs");
+var XLSX = require("xlsx");
+
+const PATHNAME = process.argv.at(-1) || ".";
+const FILENAME = "translations.xlsx";
+
+const write = (data) => {
+  Object.keys(data).forEach((key) => {
+    fs.writeFile(
+      `${PATHNAME}/${key}.json`,
+      JSON.stringify(data[key], null, 2),
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+  });
+};
+
+const workbook = XLSX.readFile(FILENAME);
+
+// number of keys
+const lang_number = Object.keys(workbook.Sheets[workbook.SheetNames[0]]).filter(
+  (v) => v.length === 2 && v.at(1) === "1"
+).length;
+const languages = workbook.Strings.slice(1, lang_number).map((v) => v.t);
+
+const translations = {};
+languages.forEach((lang) => (translations[lang] = {}));
+
+let current_key = null;
+Object.values(workbook.Sheets[workbook.SheetNames[0]])
+  .slice(1 + lang_number, -1)
+  .forEach(({ v }, i) => {
+    if (i % lang_number === 0) {
+      current_key = v;
+      return;
+    }
+    translations[languages[(i % lang_number) - 1]][current_key] = v.replaceAll(
+      "\\n",
+      "\n"
+    );
+  });
+
+write(translations);
+console.log("*Done!*");
